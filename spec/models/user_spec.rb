71e305b42956ce277257fd42a5244a3758cd22d4
twurz <travis.wurzer@gmail.com>
2014-04-24 18:57:03 -0700

    Make a basic User Model (including Secure Passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                      password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -11,6 +12,9 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -52,6 +56,19 @@ describe User do
       user_with_same_email.save
     end
 
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
 end
