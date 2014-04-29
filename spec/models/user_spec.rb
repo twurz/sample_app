06f7fa242cd3c56c0c6649348d96fcad0fcefcb6
twurz <travis.wurzer@gmail.com>
2014-04-28 23:54:46 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -71,4 +71,9 @@ describe User do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
